c1b171f8372ff294bfb4bf8d59b016bb5e34b2e7
polina.novokreschenova <noverianhenon@yandex.ru>
2020-04-23 00:03:36 +0300

    add simple messages

--- src/app.js
+++ src/app.js
@@ -3,6 +3,16 @@ import * as yup from 'yup';
 /* eslint-disable func-names */
 /* eslint no-param-reassign: ["error", { "props": false }] */
 
+// TODO: i18n
+const messages = {
+  error: {
+    network: 'Network error',
+    linkAlreadyExists: 'Link already exists',
+    invalidUrl: 'Link must be valid url',
+  },
+  success: 'RSS has been loaded',
+};
+
 yup.addMethod(yup.string, 'notAdded', function ({ message }) {
   return this.test('notAdded', message, function (link) {
     const { path, createError, options } = this;
@@ -14,8 +24,8 @@ yup.addMethod(yup.string, 'notAdded', function ({ message }) {
 const schema = yup.object().shape({
   link: yup.string()
     .required()
-    .url()
-    .notAdded({ message: 'link already exists' }),
+    .url({ message: messages.error.invalidUrl })
+    .notAdded({ message: messages.error.linkAlreadyExists }),
 });
 
 const updateValidationState = (state) => {
@@ -26,6 +36,8 @@ const updateValidationState = (state) => {
 
   schema.validate(dataToValidate, validationContext)
     .then(() => {
+      state.form.error = '';
+      state.form.isValid = true;
       state.links = [...links, data];
     })
     .catch((error) => {
