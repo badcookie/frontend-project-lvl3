15442176460dfb94cdd11df8d57fdbb2a7b9fada
polina.novokreschenova <noverianhenon@yandex.ru>
2020-05-04 22:46:21 +0300

    remove debug

--- src/parser.js
+++ src/parser.js
@@ -4,7 +4,6 @@ const getTagValue = (parentNode, tag) => parentNode.querySelector(tag).textConte
 export default (xml) => {
   const parser = new DOMParser();
   const dom = parser.parseFromString(xml, 'text/xml');
-  console.log(dom);
 
   const title = getTagValue(dom, 'title');
   const description = getTagValue(dom, 'description');
